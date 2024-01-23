9540245448df605ad9915b57d5acfd98f71c611a
Ian-o-the-bo <47067469+Ian-o-the-bo@users.noreply.github.com>
2024-01-23 14:48:09 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+namespace custom {
+    //% blockId=my_extension_name_mob_argument
+    //% block="mob argument $mob"
+    //% mob.shadow=minecraftAnimal
+    //% mob.defl=AnimalMob.Squid
+    export function mobArgument(mob: number) {
+
+    }
+
+    //% blockId=my_extension_name_monster_argument
+    //% block="monster argument $monster"
+    //% monster.shadow=minecraftMonster
+    //% monster.defl=MonsterMob.Skeleton
+    export function monsterArgument(monster: number) {
+
+    }
+
+    //% blockId=my_extension_name_block_argument
+    //% block="block argument $blockArg"
+    //% blockArg.shadow=minecraftBlock
+    //% blockArg.defl=Block.DiamondBlock
+    export function blockArgument(blockArg: number) {
+
+    }
+
+    //% blockId=my_extension_name_item_argument
+    //% block="item argument $item"
+    //% item.shadow=minecraftItem
+    //% item.defl=Item.GoldIngot
+    export function itemArgument(item: number) {
+
+    }
+}
